54dbc84e64a1bdec00e76d96f9cb0396a6fdf780
Sasha92Shevchuk <108683139+Sasha92Shevchuk@users.noreply.github.com>
2023-03-01 15:55:58 +0200

    add files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './theme';
-import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="routers-react-practice">
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
